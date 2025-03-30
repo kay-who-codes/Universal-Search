75fda806e2876b5a1f916533ea1689c016bf2ce8
Kay Who Codes <68508154+kay-who-codes@users.noreply.github.com>
2025-03-30 20:01:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,62 +3,74 @@ const searchOptions = [
     {
         name: "Google",
         icon: "fab fa-google",
-        url: "https://www.google.com/search?q="
+        url: "https://www.google.com/search?q=",
+        color: "#4285F4" // Google blue
     },
     {
         name: "YouTube",
         icon: "fab fa-youtube",
-        url: "https://www.youtube.com/results?search_query="
+        url: "https://www.youtube.com/results?search_query=",
+        color: "#FF0000" // YouTube red
     },
     {
         name: "Wikipedia",
         icon: "fab fa-wikipedia-w",
-        url: "https://en.wikipedia.org/wiki/Special:Search?search="
+        url: "https://en.wikipedia.org/wiki/Special:Search?search=",
+        color: "#FFFFFF" // Wikipedia white
     },
     {
         name: "Amazon",
         icon: "fab fa-amazon",
-        url: "https://www.amazon.com/s?k="
+        url: "https://www.amazon.com/s?k=",
+        color: "#FF9900" // Amazon orange
     },
     {
         name: "GitHub",
         icon: "fab fa-github",
-        url: "https://github.com/search?q="
+        url: "https://github.com/search?q=",
+        color: "#8B5CF6" // GitHub purple
     },
     {
         name: "Twitter",
         icon: "fab fa-twitter",
-        url: "https://twitter.com/search?q="
+        url: "https://twitter.com/search?q=",
+        color: "#1DA1F2" // Twitter blue
     },
     {
         name: "Reddit",
         icon: "fab fa-reddit",
-        url: "https://www.reddit.com/search/?q="
+        url: "https://www.reddit.com/search/?q=",
+        color: "#FF5700" // Reddit orange
     },
     {
         name: "DuckDuckGo",
         icon: "fas fa-search",
-        url: "https://duckduckgo.com/?q="
+        url: "https://duckduckgo.com/?q=",
+        color: "#DE5833" // DuckDuckGo red
     },
     {
         name: "IMDb",
         icon: "fab fa-imdb",
-        url: "https://www.imdb.com/find?q="
+        url: "https://www.imdb.com/find?q=",
+        color: "#F5C518" // IMDb yellow
     },
     {
         name: "Stack Overflow",
         icon: "fab fa-stack-overflow",
-        url: "https://stackoverflow.com/search?q="
+        url: "https://stackoverflow.com/search?q=",
+        color: "#F48024" // Stack Overflow orange
     },
     {
         name: "Spotify",
         icon: "fab fa-spotify",
-        url: "https://open.spotify.com/search/"
+        url: "https://open.spotify.com/search/",
+        color: "#1DB954" // Spotify green
     },
     {
         name: "Maps",
         icon: "fas fa-map-marker-alt",
-        url: "https://www.google.com/maps/search/"
+        url: "https://www.google.com/maps/search/",
+        color: "#34A853" // Google Maps green
     }
 ];
 
@@ -82,7 +94,7 @@ function renderSearchOptions() {
         optionElement.className = 'search-option';
         optionElement.dataset.url = option.url;
         optionElement.innerHTML = `
-            <i class="${option.icon}"></i>
+            <i class="${option.icon}" style="color: ${option.color}"></i>
             <span class="name">${option.name}</span>
         `;
         searchOptionsContainer.appendChild(optionElement);
